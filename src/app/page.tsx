e1ecf6c0ef6bcd14d068a6dd8ffd442a17045b83
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2025-01-02 19:37:58 -0300

    feat(main) language detectin and redirect (#2)
    * feat(main) language detectin and redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import { langList } from '@/conf';
 import { Link } from '@mui/material';
 import NextLink from 'next/link';
+import { Redirect } from '@/client/components/redirect';
 
 export default function Home() {
   return (
-    <div className="h-[100vh] flex justify-center items-center">
+    <div className="h-[100vh] flex justify-center flex-col items-center">
+      <Redirect />
       <div className="flex flex-row gap-4 text-4xl">
         {langList.map((lang) => (
-          <Link component={NextLink} key={`lang-${lang}`} href={`/${lang}`}>{`/${lang}`}</Link>
+          <Link component={NextLink} key={`lang-${lang}`} href={`/${lang}`}>{`${lang}`}</Link>
         ))}
       </div>
     </div>
