312c4f301b64ff0cf882bf9826fdf861e8ea5568
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2025-01-03 01:07:53 -0300

    fix(deploy) CI/CDf fix (#4)
    fix(deploy) CI/CDf fix

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -18,4 +18,4 @@ export const getI18n = (lang: string): I18nLocaleInterface => {
 };
 
 export const replaceTextVars = (string: string, object: Record<string, string>) =>
-  string.replaceAll(/\{([^}]+)\}/gi, (_, a) => a.split('.').reduce((b, c) => b?.[c], object));
+  string.replaceAll(/\{([^}]+)\}/gi, (_, a) => a.split('.').reduce((b: string, c: number) => b?.[c], object));
