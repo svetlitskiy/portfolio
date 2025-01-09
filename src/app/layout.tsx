bc9bc6db53e68faf0e52fc7b2ae88ccb013adfd0
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2025-01-09 00:45:28 -0300

    feat(yc-metrica) add yandex metrica

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,6 @@
 import type { Metadata } from 'next';
-import { Ubuntu_Mono } from 'next/font/google';
 import './globals.css';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
-
-const ubuntuMono = Ubuntu_Mono({
-  subsets: ['cyrillic', 'latin'],
-  weight: '400',
-});
 
 export const metadata: Metadata = {
   title: 'Aleksey Svetlitskiy',
@@ -18,11 +12,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${ubuntuMono.className} bg-background`}>
-        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
-      </body>
-    </html>
-  );
+  return <AppRouterCacheProvider>{children}</AppRouterCacheProvider>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,17 @@ import { Redirect } from '@/client/components/redirect';
 
 export default function Home() {
   return (
-    <div className="h-[100vh] flex justify-center flex-col items-center">
-      <Redirect />
-      <div className="flex flex-row gap-4 text-4xl">
-        {langList.map((lang) => (
-          <Link component={NextLink} key={`lang-${lang}`} href={`/${lang}`}>{`${lang}`}</Link>
-        ))}
-      </div>
-    </div>
+    <html lang="en">
+      <body className="bg-background text-default">
+        <div className="h-[100vh] flex justify-center flex-col items-center">
+          <Redirect />
+          <div className="flex flex-row gap-4 text-4xl">
+            {langList.map((lang) => (
+              <Link component={NextLink} key={`lang-${lang}`} href={`/${lang}`}>{`${lang}`}</Link>
+            ))}
+          </div>
+        </div>
+      </body>
+    </html>
   );
 }
