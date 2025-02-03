090c788320f9e9486fcc5257ee478d92c4f8d12b
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2025-02-03 10:39:02 -0300

    feat(cv) print version

--- src/app/[lang]/resume/theme-resume.ts
+++ src/app/[lang]/resume/theme-resume.ts
@@ -9,21 +9,47 @@ export const themeResume = createTheme({
       fontSize: '3rem',
       fontWeight: 500,
       color: 'black',
+      '@media print': {
+        fontWeight: 700,
+        fontSize: '18pt',
+        fontFamily: `'EB Garamond', serif`,
+      },
     },
     h2: {
+      fontWeight: 500,
       fontSize: '2rem',
       color: 'black',
+      '@media print': {
+        fontWeight: 700,
+        fontSize: '16pt',
+        fontFamily: `'EB Garamond', serif`,
+      },
     },
+
     h3: {
+      fontWeight: 500,
       fontSize: '1.8rem',
+      '@media print': {
+        fontWeight: 700,
+        fontSize: '14pt',
+        fontFamily: `'EB Garamond', serif`,
+      },
     },
     body1: {
-      fontWeight: 500,
+      fontWeight: 400,
       fontSize: '1.2rem',
+      '@media print': {
+        fontSize: '12pt',
+        fontFamily: `'EB Garamond', serif`,
+      },
     },
     body2: {
-      fontWeight: 500,
+      fontWeight: 400,
       fontSize: '1.1rem',
+      '@media print': {
+        fontSize: '10pt',
+        fontFamily: `'EB Garamond', serif`,
+      },
     },
   },
 });
