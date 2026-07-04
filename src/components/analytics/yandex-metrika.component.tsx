653e3b6554c889a8215c112b573cd057ff42f6dc
Aleksey Svetlitskiy <a.svetlitskiy@gmail.com>
2026-07-04 20:38:56 -0300

    fix: document yandex metrika setup (#24)

--- src/components/analytics/yandex-metrika.component.tsx
+++ src/components/analytics/yandex-metrika.component.tsx
@@ -1,13 +1,19 @@
-'use client'
+'use client';
 
-import Script from 'next/script'
-import React from 'react'
+import Script from 'next/script';
+import React from 'react';
 
 interface YandexMetrikaProps {
-  id: string
+  id: string;
 }
 
 export const YandexMetrika: React.FC<YandexMetrikaProps> = ({ id }) => {
+  const counterId = id.trim();
+
+  if (!/^\d+$/.test(counterId)) {
+    return null;
+  }
+
   return (
     <>
       <Script
@@ -20,21 +26,21 @@ export const YandexMetrika: React.FC<YandexMetrikaProps> = ({ id }) => {
               m[i].l=1*new Date();
               for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
               k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=` + id + `', 'ym');
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${counterId}', 'ym');
 
-            ym(` + id + `, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+            ym(${counterId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
           `,
         }}
       />
       <noscript>
         <div>
           <img
-            src={`https://mc.yandex.ru/watch/${id}`}
+            src={`https://mc.yandex.ru/watch/${counterId}`}
             style={{ position: 'absolute', left: '-9999px' }}
             alt=""
           />
         </div>
       </noscript>
     </>
-  )
-}
+  );
+};
